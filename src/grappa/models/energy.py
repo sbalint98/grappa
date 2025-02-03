Fix wrong morse potential formula, Use proper intials

src/grappa/models/energy.py
@@ -67,7 +67,7 @@ def morse_energy(de, eq, a, distances):
         raise ValueError(f"de, eq and a must be a 1d tensors but has shapes de: {de.shape}, eq: {eq.shape} and a: {a.shape}")
     pass
 
-    energy = torch.square((1-torch.exp(a.unsqueeze(dim=-1)-(distances-eq.unsqueeze(dim=-1)))))
+    energy = torch.square((1-torch.exp(-a.unsqueeze(dim=-1)*(distances-eq.unsqueeze(dim=-1)))))
     return de.unsqueeze(dim=-1)*energy
     
     

src/grappa/models/interaction_parameters.py
@@ -236,10 +236,10 @@ class WriteBondParameters(torch.nn.Module):
             k_std=param_statistics["std"]["n2_k"].item() + EPSILON_STD
             eq_mean=param_statistics["mean"]["n2_eq"].item()
             eq_std=param_statistics["std"]["n2_eq"].item() + EPSILON_STD
-            de_mean=1.
-            de_std=0.
-            a_mean=1.
-            a_std=0.
+            de_mean=param_statistics["mean"]["n2_de"].item()
+            de_std=param_statistics["std"]["n2_de"].item() + EPSILON_STD
+            a_mean=param_statistics["mean"]["n2_a"].item()
+            a_std=param_statistics["std"]["n2_a"].item() + EPSILON_STD
             
         else:
             k_mean = 0.